74cbd1fc41e3b917ab40ca73bd897a5dcfafcc87
Luca Spalierno <spalierno@gmail.com>
2020-09-27 23:31:37 +0200

    Add homework 6 - basic.

--- homework6/main.py
+++ homework6/main.py
@@ -2,37 +2,35 @@ from core.opponents import Opponents
 from core.spaceship import Spaceship
 
 
-class Main:
-    def __init__(self):
-        round_number = 1
+def parse_details(details_string: str):
+    details_string.replace(' ', '')
+    parsed_details = details_string.split(',')
 
-    def parse_details(self, details_string: str):
-        details_string.replace(' ', '')
-        parsed_details = details_string.split(',')
+    if len(parsed_details) != 3:
+        raise ValueError('')
 
-        if len(parsed_details) != 3:
-            raise ValueError('')
+    return list(map(int, parsed_details))
 
-        return list(map(int, parsed_details))
 
-    def init(self):
-        battleship_one = self.battleship_from_cli(1)
-        battleship_two = self.battleship_from_cli(2)
-        print('\n')
+def init():
+    battleship_one = battleship_from_cli(1)
+    battleship_two = battleship_from_cli(2)
+    return Opponents([battleship_one, battleship_two])
 
-        return Opponents([battleship_one, battleship_two])
 
-    def battleship_from_cli(self, battleship_number: int):
-        battleship_name = input(f'Enter battleship {battleship_number} name: ')
-        battleship_details = input('Enter battleship 1 details (HP, Armor, Damage) separated by commas:')
-        parsed_details = self.parse_details(battleship_details)
+def battleship_from_cli(battleship_number: int):
+    battleship_name = input(f'Enter battleship {battleship_number} name: ')
+    battleship_details = input('Enter battleship 1 details (HP, Armor, Damage) separated by commas:')
+    parsed_details = parse_details(battleship_details)
 
-        return Spaceship(name=battleship_name, hp=parsed_details[0], armor=parsed_details[1],
-                         damage=parsed_details[2])
+    return Spaceship(name=battleship_name, hp=parsed_details[0], armor=parsed_details[1],
+                     damage=parsed_details[2])
 
 
 if __name__ == '__main__':
-    Main().init()
+    opponents = init()
+    print('\n')
+    round_number = 1
 
     # cover edge cases (e.g. battle can't begin)
     while opponents.should_continue():

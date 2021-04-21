61a263c47331b21e36daa3d05702f76b08e5f644
Luca Spalierno <spalierno@gmail.com>
2021-04-21 22:29:29 +0200

    Add the possibility to pick a random amount of flowers.

--- flower-garden/main.py
+++ flower-garden/main.py
@@ -1,15 +1,26 @@
+import random
+from typing import List
+
 from experiment import Experiment
+from flowers.base.flower import Flower
 from flowers.blue_flower import BlueFlower
 from flowers.red_flower import RedFlower
 from flowers.yellow_flower import YellowFlower
 
 
 def main():
-    flowers = [YellowFlower(), RedFlower(), BlueFlower()]
+    number_of_flowers = 10
     number_of_days = 1000
+
+    flowers = _pick_flowers(number_of_flowers)
 
     experiment = Experiment(days=number_of_days, flowers=flowers)
     experiment.run()
+
+
+def _pick_flowers(number: int) -> List[Flower]:
+    flower_types = [YellowFlower, RedFlower, BlueFlower]
+    return [random.choice(flower_types)() for _ in range(number)]
 
 
 if __name__ == '__main__':
